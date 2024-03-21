1764156269ea5910bbb1497b744e697a60ee0ffc
jahangir shah <jahangirshahofficial@gmail.com>
2024-03-22 00:28:36 +0500

    merge

---
+++ public/assets/js/ajax/ajax.js
@@ -0,0 +1,34 @@
+
+  // update  tag stage
+  $('.draggable').on('dragend', function() {
+    let id = 1;
+    let taskId = $(this).attr('data-task-id');
+    let taskStage = $(this).attr('data-task-stage');
+    let _token = $('meta[name="csrf-token"]').attr('content');
+    let url = baseUrl + ":8000/task/move/" + id;
+    ajaxCRUD("PUT", url, { taskId, taskStage, _token }, (response) => {
+        console.log(response);
+  });
+  });
+
+
+/**
+ * Helper functions to avoid repetition 
+ */
+
+// reusable ajax function
+function ajaxCRUD(method, url, data, successFunc) {
+$.ajax({
+  type: method,
+  url: url,
+  data: data,
+  success: function (response) {
+    if (successFunc) {
+      successFunc(response);
+    }
+  },
+  error: function (xhr, status, error) {
+    console.error(xhr.responseText);
+  },
+});
+}
---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,46 @@
+@extends('master')
+@section('content')
+
+    <section class="section">
+        <div class="section-header ">
+            <h1>Board</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="#">Components</a></div>
+                <div class="breadcrumb-item">Board</div>
+            </div>
+        </div>
+
+
+    <div class="section-body task-main  h-100  ">
+        <table class="table table-light col-12 ">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>No of Actie Tasks</th>
+                    <th>Completed</th>
+                    <th>Pending</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($show as $item)
+
+
+                <tr>
+                    <td>{{$item->name}}</td>
+                    <td>{{$item->name}}</td>
+                    <td>{{$item->name}}</td>
+                    <td>{{$item->name}}</td>
+                    <td>{{$item->name}}</td>
+                    <td></td>
+                </tr>
+                @empty
+
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+
+@endsection
